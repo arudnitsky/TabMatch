2ff06edda80c4279daa8bca3bbcd954aae242284
Andy Rudnitsky <a.rudnitsky@techsmith.com>
2017-10-13 21:26:57 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,88 @@
+const saveLocationFolderTitle = 'TabSaver';
+const notFoundId = -1;
+
+
+logAllTabs = function () {
+    console.log('----------------------------------------------------------------------');
+    chrome.windows.getAll({populate:true},function(windows){
+        windows.forEach(function(window){
+            console.log('Window -------------');
+            window.tabs.forEach(function(tab){
+                console.log(tab.url);
+          });
+        });
+    
+    });
+};
+
+createBookmark = function(parentId, tab) {
+    chrome.bookmarks.create(
+        {'parentId': parentId, 'title': tab.title, 'url': tab.url}
+        // , function(newBookmark) {console.log("added bookmark: " + newBookmark.title);}
+    );
+};
+
+saveTabsToSaveLocation = function (saveLocationId) {
+    chrome.windows.getAll({populate:true},function(windows){
+        windows.forEach(function(window){
+            window.tabs.forEach(function(tab){
+                createBookmark(saveLocationId, tab);
+          });
+        });
+    
+    });
+};
+
+createSaveLocation = function(parentId) {
+    chrome.bookmarks.create({'parentId': parentId, 'title': 'Extension bookmarks'});
+};
+
+findIdForTitle = function(bookmarks, titleToFind) {
+    var found = false;
+    var idOfTitle = -1;
+
+    findIdForTitleInternal = function(bookmarks, titleToFind) {
+        for (var ii = 0; ii < bookmarks.length; ++ii) {
+            var bookmark = bookmarks[ii];
+            if (bookmark.title === titleToFind) {
+                found = true;
+                idOfTitle = bookmark.id;
+                break;
+            }
+            if (bookmark.children) {
+                findIdForTitleInternal(bookmark.children, titleToFind);
+                if (found) {
+                    break;
+                }
+            }
+        }    
+    }
+
+    findIdForTitleInternal(bookmarks, titleToFind);
+    return idOfTitle;
+}
+   
+doSaveTabs = function () {
+    chrome.bookmarks.getTree(function(bookmarks) {
+        // if has save location saveLocation
+        //     delete it
+        //     recreate it
+        // endif
+
+        var saveLocationId = findIdForTitle(bookmarks, saveLocationFolderTitle);
+        if (saveLocationId != notFoundId) {
+            // deleteSaveLocation(saveLocationId);
+        }
+        // createSaveLocation(saveLocationFolderTitle);
+        saveTabsToSaveLocation(saveLocationId);
+    });
+};
+
+// chrome.alarms.create( "myAlarm", { delayInMinutes: 1, periodInMinutes: 1});
+// chrome.alarms.onAlarm.addListener(doSaveTabs);
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('container').addEventListener("click", doSaveTabs );
+});
+
+
