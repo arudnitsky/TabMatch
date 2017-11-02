44bb631955a2e5ee13eb23632d3e3645caf6fee9
Andy Rudnitsky <a.rudnitsky@techsmith.com>
2017-11-02 14:46:58 -0400

    Move tab saving into event page

---
+++ src/background.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const saveLocationParentFolderTitle = 'Other bookmarks';
+const saveLocationFolderTitle = 'TabMatch-Macbook';
+const notFoundId = -1;
+
+var createBookmark = function(parentId, tab) {
+   chrome.bookmarks.create({parentId: parentId, title: tab.title, url: tab.url});
+};
+
+var saveTabsToSaveLocation = function(saveLocationId) {
+   chrome.windows.getAll({
+      populate: true
+   }, function(windows) {
+      windows.forEach(function(window) {
+         window.tabs.forEach(function(tab) {
+            createBookmark(saveLocationId, tab);
+         });
+      });
+   });
+};
+
+var findIdForTitle = function(bookmarks, titleToFind) {
+   var found = false;
+   var idOfTitle = -1;
+
+   var findIdForTitleInternal = function(internalBookmarks, internalTitleToFind) {
+      for (var ii = 0; ii < internalBookmarks.length; ++ii) {
+         var bookmark = internalBookmarks[ii];
+         if (bookmark.title.toUpperCase() === internalTitleToFind.toUpperCase()) {
+            found = true;
+            idOfTitle = bookmark.id;
+            break;
+         }
+         if (bookmark.children) {
+            findIdForTitleInternal(bookmark.children, internalTitleToFind);
+            if (found) {
+               break;
+            }
+         }
+      }
+   };
+
+   findIdForTitleInternal(bookmarks, titleToFind);
+   return idOfTitle;
+};
+
+var doSaveTabs = function() {
+   chrome.bookmarks.getTree(function(bookmarks) {
+      var saveLocationParentId = findIdForTitle(bookmarks, saveLocationParentFolderTitle);
+      if (saveLocationParentId === notFoundId) {
+         alert('Can\'t find ', saveLocationFolderTitle);
+         return;
+      }
+
+      var saveLocationId = findIdForTitle(bookmarks, saveLocationFolderTitle);
+      if (saveLocationId !== notFoundId) {
+         chrome.bookmarks.removeTree(saveLocationId);
+      }
+
+      chrome.bookmarks.create(
+         {parentId: saveLocationParentId, title: saveLocationFolderTitle},
+         function(results) {
+            saveTabsToSaveLocation(results.id);
+         }
+      );
+   });
+};
+
+chrome.alarms.onAlarm.addListener(function() {
+   doSaveTabs();
+});
+
+chrome.runtime.onMessage.addListener(function(msg, sender) {
+   alert(msg.message);
+});
--- src/main.js
+++ src/main.js
@@ -1,75 +1,11 @@
-const saveLocationParentFolderTitle = 'Other bookmarks';
-const saveLocationFolderTitle = 'TabMatch-Windows';
-const notFoundId = -1;
+'use strict';
 
-createBookmark = function(parentId, tab) {
-   chrome.bookmarks.create({parentId: parentId, title: tab.title, url: tab.url}
-      // , function(newBookmark) {console.log("added bookmark: " + newBookmark.title);}
-   );
+var sendClickMessage = function() {
+   chrome.runtime.sendMessage({message: 'saveNowClick'});
 };
-
-saveTabsToSaveLocation = function(saveLocationId) {
-   chrome.windows.getAll({
-      populate: true
-   }, function(windows) {
-      windows.forEach(function(window) {
-         window.tabs.forEach(function(tab) {
-            createBookmark(saveLocationId, tab);
-         });
-      });
-   });
-};
-
-findIdForTitle = function(bookmarks, titleToFind) {
-   var found = false;
-   var idOfTitle = -1;
-
-   findIdForTitleInternal = function(internalBookmarks, internalTitleToFind) {
-      for (var ii = 0; ii < internalBookmarks.length; ++ii) {
-         var bookmark = internalBookmarks[ii];
-         if (bookmark.title.toUpperCase() === internalTitleToFind.toUpperCase()) {
-            found = true;
-            idOfTitle = bookmark.id;
-            break;
-         }
-         if (bookmark.children) {
-            findIdForTitleInternal(bookmark.children, internalTitleToFind);
-            if (found) {
-               break;
-            }
-         }
-      }
-   };
-
-   findIdForTitleInternal(bookmarks, titleToFind);
-   return idOfTitle;
-};
-
-doSaveTabs = function() {
-   chrome.bookmarks.getTree(function(bookmarks) {
-      var saveLocationParentId = findIdForTitle(bookmarks, saveLocationParentFolderTitle);
-      if (saveLocationParentId === notFoundId) {
-         alert('Can\'t find ', saveLocationFolderTitle);
-         return;
-      }
-
-      var saveLocationId = findIdForTitle(bookmarks, saveLocationFolderTitle);
-      if (saveLocationId !== notFoundId) {
-         chrome.bookmarks.removeTree(saveLocationId);
-      }
-
-      chrome.bookmarks.create(
-         {parentId: saveLocationParentId, title: saveLocationFolderTitle},
-         function(results) {
-            saveTabsToSaveLocation(results.id);
-         }
-      );
-   });
-};
-
-// chrome.alarms.create( "myAlarm", { delayInMinutes: 1, periodInMinutes: 1});
-// chrome.alarms.onAlarm.addListener(doSaveTabs);
 
 document.addEventListener('DOMContentLoaded', () => {
-   document.querySelector('#save-tabs-button').addEventListener('click', doSaveTabs);
+   document.querySelector('#save-tabs-button').addEventListener('click', sendClickMessage);
 });
+
+chrome.alarms.create({delayInMinutes: 1, periodInMinutes: 5});
